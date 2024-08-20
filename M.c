f5e8aa4d01ad2de544c597e3c0d3337345c4207b
sanket124w <waghmaresanket124@gmail.com>
2024-08-20 22:48:27 +0530

    multiplication of two number

---
+++ M.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int a;
+    int b;
+    printf("Enter first number : ");
+    scanf("%d", a);
+    printf("Enter second number : ");
+    scanf("%d", b);
+    int sum;
+    sum = a * b;
+    printf("Substraction is : %d", sum);
+    return 0;
+}
