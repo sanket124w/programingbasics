ff000ac80741bde876e1542e81030664b011fcf3
sanket124w <161968547+sanket124w@users.noreply.github.com>
2024-08-20 22:37:09 +0530

    Create M.c

---
+++ M.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a;
+    int b;
+    printf("Enter first number : ");
+    scanf("%d", a);
+    printf("Enter second number : ");
+    scanf("%d", b);
+    int sum;
+    printf("Substraction is : %d", sum);
+    return 0;
+}
